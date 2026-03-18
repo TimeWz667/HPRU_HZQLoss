52ec727ee2a530cab47a433119749173b84506e9
Chu-Chang Ku <timewz667@gmail.com>
2026-03-18 19:06:58 +0000

    initialise targets

---
+++ _targets.R
@@ -0,0 +1,30 @@
+library(targets)
+library(tarchetypes) # Load other packages as needed.
+
+
+
+
+
+
+
+# Set target options:
+tar_option_set(
+  packages = c("tidyverse", "rstan")
+)
+
+# Run the R scripts in the R/ folder with your custom functions:
+tar_source()
+# tar_source("other_functions.R") # Source other scripts as needed.
+
+# Replace the target list below with your own:
+list(
+  tar_target(
+    name = data,
+    command = tibble(x = rnorm(100), y = rnorm(100))
+    # format = "qs" # Efficient storage for general data objects.
+  ),
+  tar_target(
+    name = model,
+    command = coefficients(lm(y ~ x, data = data))
+  )
+)
