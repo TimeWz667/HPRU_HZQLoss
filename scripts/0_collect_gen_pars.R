bc4af4b84fc0237f2ea6d4e41a9a5e862d6b7522
Chu-Chang Ku <timewz667@gmail.com>
2026-04-29 23:09:33 +0100

    Add gsk inputs

---
+++ scripts/0_collect_gen_pars.R
@@ -0,0 +1,179 @@
+library(tidyverse)
+
+
+list_studies <- read_csv(here::here("data", "list_studies.csv"))
+
+
+
+root <- here::here("data", "inputs_for_gen")
+
+dir.create(root, showWarnings = F)
+
+
+
+## TTE sub
+ds <- dir(here::here("data", "inputs_r1_post"))
+ds <- ds[startsWith(ds, "stats_tte_sub")]
+
+ds
+
+pars_tte_r1 <- bind_rows(lapply(ds, \(f) {
+  if (f == "RamCRI") {
+    tab <- read_csv(here::here("data", "inputs_r1_post", f)) %>% 
+      filter(Var %in% c("r0", "ba1")) %>% 
+      mutate(SID = f)
+  } else {
+    tab <- read_csv(here::here("data", "inputs_r1_post", "stats_ph_tte.csv")) %>% 
+      filter(Var %in% c("r0", "ba1")) %>% 
+      mutate(SID = f)
+  }
+  
+tab
+})) %>% 
+  extract(SID, "SID", "stats_tte_sub_(\\w+).csv")
+
+
+pars_tte_gsk <- src_tte_gsk %>% 
+  filter(Var %in% c("r0", "ba1")) %>% 
+  filter(SID %in% c("DEU", "ESP", "JPN", "ITA"))
+
+
+pars_tte <- bind_rows(pars_tte_r1, pars_tte_gsk) %>% 
+  select(SID, Var, mean, sd)
+
+
+
+write_csv(pars_tte, here::here(root, "pars_tte_ind.csv"))
+
+
+
+pars_tte_r1_ph <- read.csv(here::here("data", "inputs_r1_post", "stats_ph_tte.csv"))
+
+
+pars_tte <- bind_rows(
+    pars_tte_r1 %>% 
+      select(SID, Var) %>% 
+      left_join(pars_tte_r1_ph), 
+    pars_tte_gsk %>% 
+      select(SID, Var) %>% 
+      left_join(src_tte_gsk %>% filter(SID == "GSK") %>% select( - SID))
+  ) %>% 
+  select(SID, Var, mean, sd)
+
+write_csv(pars_tte, here::here(root, "pars_tte_2gp.csv"))
+
+
+
+
+## TTE slopes
+src_slopes_r1 <- read_csv(here::here("data", "inputs_r1_post", "stats_tte_slopes.csv")) %>% 
+  select(SID, Var, m) %>% 
+  pivot_wider(names_from = Var, values_from = m)
+
+src_slopes_gsk <- read_csv(here::here("data", "inputs_gsk_post", "Summary_slopes.csv")) %>% 
+  extract(file, "SID", "Post_tte_(\\w+).csv")
+
+
+pars_slopes <- bind_rows(
+  src_slopes_r1 %>% 
+    filter(SID != "RamCRI") %>% 
+    mutate(SID = ifelse(SID == "Combined", "RamCRI", SID)),
+  src_slopes_gsk %>% 
+    filter(SID %in% c("DEU", "ESP", "JPN", "ITA"))
+)
+
+
+write_csv(pars_slopes, here::here(root, "pars_tte_slopes_ind.csv"))
+
+
+
+pars_slopes <- bind_rows(
+  src_slopes_r1 %>% select(SID) %>% 
+    filter(SID != "Combined") %>% 
+    bind_cols(src_slopes_r1 %>% filter(SID == "Combined") %>% select(-SID)),
+  src_slopes_gsk %>% select(SID) %>% 
+    filter(SID %in% c("DEU", "ESP", "JPN", "ITA")) %>% 
+    bind_cols(src_slopes_gsk %>% filter(SID == "gsk") %>% select(-SID))
+)
+
+write_csv(pars_slopes, here::here(root, "pars_tte_slopes_2gp.csv"))
+
+
+
+
+
+### QOL
+
+
+
+pars_qol_r1 <- list_studies %>% 
+  filter(StudyGroup == "r1") %>% 
+  pull(SID) %>% 
+  lapply(\(sid) {
+    bind_rows(
+      read_csv(here::here("data", "inputs_r1_post", paste0("stats_qol_b_orig_sub_", sid, ".csv"))) %>% 
+        filter(Var != "lp__") %>% 
+        mutate(Var = paste0(Model, "_", Var)),
+      read_csv(here::here("data", "inputs_r1_post", paste0("stats_qol_b_bg_orig_sub_", sid, ".csv"))) %>% 
+        filter(Var != "lp__") %>% 
+        mutate(Var = gsub("\\[(\\d+)\\]", "", paste0(Model, "_", Var)))
+    ) %>% 
+      select(Model, Var, mean, sd) %>% 
+      mutate(SID = sid)
+  }) %>% 
+  bind_rows() %>% 
+  arrange(SID, Model, Var)
+
+
+pars_qol_r1
+
+
+pars_qol_gsk0 <- read_csv(here::here("data", "inputs_gsk_post", "Summary_qols.csv")) %>% 
+  distinct() %>% 
+  filter(Var != "lp__") %>% 
+  mutate(Var = gsub("\\[1\\]", "", Var)) %>% 
+  filter(SID %in% c("DEU", "ESP", "JPN", "ITA")) %>% 
+  mutate(Var = paste0(Model, "_", Var)) %>% 
+  select(SID, Model, Var, mean, sd)
+
+
+pars_qol <- bind_rows(pars_qol_r1, pars_qol_gsk0)
+
+
+write_csv(pars_qol, here::here(root, "pars_qol.csv"))
+
+
+
+
+## Age distribution
+
+prof_a_r1 <- read_csv(here::here("data", "inputs_studies", "tab_agp_by_studies.csv"))
+prof_a_r1 <- prof_a %>% rename(study = SID) %>% left_join(list_studies) %>% select(-c(study, SIK)) %>% 
+  mutate(SID = factor(SID, list_studies$SID))
+
+prof_a_gsk <- read_csv(here::here("data", "inputs_gsk_post", "tab_cnt.csv"))
+
+
+prof_a <- bind_rows(
+  prof_a %>% 
+    filter(!is.na(Agp)) %>% 
+    select(study = SID, Agp, n) %>% 
+    tidyr::extract(Agp, c("A0", "A1"), "\\[(\\d+),(\\d+)\\)", convert = T, remove = F) %>% 
+    left_join(list_studies %>% select(study, SID)) %>% 
+    select(SID, Agp, A0, A1, n),
+  prof_a_gsk %>% 
+    mutate(
+      SID = case_when(
+        value_set == "Germany" ~ "DEU", 
+        value_set == "Japan"~ "JPN",
+        value_set == "Italy" ~ "ITA",
+        value_set == "Spain" ~ "ESP",
+        T ~ NA
+      )
+    ) %>% 
+    select(SID, Agp, A0 = age_min, A1 = age_max, n = n_sub)
+)
+
+
+
+write_csv(prof_a, here::here(root, "prof_a.csv"))
---
+++ scripts/3_generate_full_data.R
@@ -0,0 +1,144 @@
+library(tidyverse)
+library(rstan)
+
+
+## Function
+
+gen_patients <- function(prof, pars_qol, pars_tte, pars_tte_slopes) {
+  data_gen <- prof %>% 
+    uncount(n) %>% 
+    mutate(
+      PID = paste0(SID, "_", 1:n()),
+      A0 = pmax(A0, 18),
+      Age = floor(runif(n(), A0, A1))
+    ) %>% 
+    select(-c("Agp", "A0", "A1")) %>% 
+    full_join(bind_rows(pars_tte, pars_qol) %>% select(- Model), by = "SID", relationship = "many-to-many")  %>% 
+    mutate(value = rnorm(n(), mean, sd)) %>% 
+    select(-c(mean, sd)) %>% 
+    pivot_wider(names_from = Var, values_from = value) %>% 
+    left_join(pars_tte_slopes) %>% 
+    mutate(
+      r0 = pmax(r0, 0.0001),
+      ba1_sim = b0 + log(r0) * b_lr0 + rnorm(n(), 0, rsd),
+      rate = r0 * exp(Age * ba1_sim),
+      disuti = rexp(n(), rate = rate)
+    ) %>% 
+    crossing(ti = time_steps) %>% 
+    arrange(SID, PID, ti) %>% 
+    mutate(
+      time_points = ti * 365.25,
+      Q_0 = 1,
+      Q_1 = pmin(rnorm(n(), C1_b0 + C1_bg * ti, C1_sigma), 1),
+      Q_2 = pmin(rnorm(n(), C2_b0 + C2_bg * ti, C2_sigma), 1),
+      PZ = PZ_b0 + PZ_bd15 * (ti <= 15 / 365.25) + PZ_bd30 * (ti <= 30 / 365.25) + PZ_bg,
+      PZ = 1 / (1 + exp(-PZ)),
+      PC1 = PC1_b0 + PC1_bd15 * (ti <= 15 / 365.25) + PC1_bd30 * (ti <= 30 / 365.25) + PC1_bg,
+      PC1 = 1 / (1 + exp(-PC1))
+    ) %>% 
+    group_by(SID, PID) %>%  
+    mutate(
+      visit = 1:n(),
+      last = ti == max(ti < disuti),
+      PZ = ifelse(last, 0, PZ),
+      Prop_0 = PZ,
+      Prop_1 = PC1 * (1 - PZ),
+      Prop_2 = 1 - Prop_0 - Prop_1,
+      seed = runif(n()),
+      EQ5D = case_when(
+        ti > disuti ~ Q_0, 
+        seed < Prop_0 ~ Q_0,
+        seed < Prop_0 + Prop_1 ~ Q_1,
+        T ~ Q_2
+      )
+    ) %>% 
+    ungroup() %>% 
+    select(
+      SID, PID, Age, visit, ti, time_points, EQ5D, disuti, ba1_sim, r0, rate
+    )
+  
+  return(data_gen)
+}
+
+
+## Background population
+list_studies <- read_csv(here::here("data", "list_studies.csv")) %>% 
+  select(study, SID, SIK)
+list_studies
+
+
+root_src <- here::here("data", "inputs_for_gen")
+
+
+
+## Time steps 
+time_steps <- (seq(0, by = 14, length.out = 20) + 1) / 365.25
+
+
+## Shared inputs
+
+prof_a <- read_csv(here::here(root_src, "prof_a.csv"))
+
+pars_qol <- read_csv(here::here(root_src, "pars_qol.csv"))
+
+set.seed(11667)
+seeds <- round(runif(100, 0, 1e6))
+
+names(seeds) <- paste0("s", 1:length(seeds))
+
+
+## Scenario: individual tte
+pars_tte <- read_csv(here::here(root_src, "pars_tte_ind.csv"))
+pars_tte_slopes <- read_csv(here::here(root_src, "pars_tte_slopes_ind.csv"))
+
+
+root_tar <- here::here("out", "gen_ind")
+
+dir.create(root_tar, showWarnings = F )
+
+
+for (v in names(seeds)) {
+  set.seed(seeds[v])
+  print(seeds[v])
+  
+  data_gen <- gen_patients(prof_a, pars_qol, pars_tte, pars_tte_slopes) %>% 
+    mutate(
+      SID = factor(SID, list_studies$SID)
+    ) %>% 
+    arrange(SID, PID, visit)
+  
+  
+  write_csv(data_gen, file = here::here(root_tar, "generated_" + glue::as_glue(v) + ".csv"))
+  
+}
+
+
+## Scenario: two group tte
+pars_tte <- read_csv(here::here(root_src, "pars_tte_2gp.csv"))
+pars_tte_slopes <- read_csv(here::here(root_src, "pars_tte_slopes_2gp.csv"))
+
+
+root_tar <- here::here("out", "gen_2gp")
+
+dir.create(root_tar, showWarnings = F )
+
+
+
+for (v in names(seeds)) {
+  set.seed(seeds[v])
+  print(seeds[v])
+  
+  data_gen <- gen_patients(prof_a, pars_qol, pars_tte, pars_tte_slopes) %>% 
+    mutate(
+      SID = factor(SID, list_studies$SID)
+    ) %>% 
+    arrange(SID, PID, visit)
+  
+  
+  write_csv(data_gen, file = here::here(root_tar, "generated_" + glue::as_glue(v) + ".csv"))
+  
+}
+
+
+
+
